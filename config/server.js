ee1a3dc2cd7dcf3802f8caf92d9dc9ea698e6d30
Yan Real <real.yan.yr@gmail.com>
2021-04-15 07:15:13 -0300

    Creating E-Commerce Strapi API project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e484dd61345f3be5edbc5a840fa4e71c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '84f1d243-ef71-41ab-8849-116632c00cb8'
+};
